952b0a672304e0a1d98caf41e857dba345e211ba
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2024-03-30 14:55:32 -0500

    Update combo.py

--- combo.py
+++ combo.py
@@ -112,7 +112,7 @@ def region_of_interest(image):
 
 # Initialize video capture and buttons
 # Initialize video capture and buttons
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
